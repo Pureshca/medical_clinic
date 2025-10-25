16543c74caf4ce8512db9198dccf8a7da81446ff
svetlana1212 <svetik.kosareva2004@mail.ru>
2025-10-26 01:21:52 +0300

    build

--- app.py
+++ app.py
@@ -712,4 +712,7 @@ def handle_exception(e):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    # Всегда запускаем сервер
+    if os.getenv("FLASK_ENV") == "production":
+        print("Starting Flask production server...")
+        app.run(host="0.0.0.0", port=5000, debug=False)
