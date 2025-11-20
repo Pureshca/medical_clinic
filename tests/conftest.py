4c0022f791a070abc497c0ef966ca0cc26ba83ff
Pureshca <146196919+Pureshca@users.noreply.github.com>
2025-11-20 13:07:02 +0000

    fix_test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,16 +5,24 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 import pytest
+from app import app as flask_app  # используем существующий объект app
 from models import db, populate_db
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def app():
-    with app.app_context():
+    # Используем существующее приложение
+    flask_app.config.update({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",  # in-memory база для тестов
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+    })
+
+    with flask_app.app_context():
         db.drop_all()
         db.create_all()
         populate_db()
 
-    yield app
+    yield flask_app
 
 @pytest.fixture()
 def client(app):
