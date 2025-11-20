b43dfade6fd90a62a6377bf5d3460074dce6a950
Pureshca <146196919+Pureshca@users.noreply.github.com>
2025-11-20 14:17:46 +0300

    Delete tests directory

--- tests/integration/test_healthcheck.py
+++
@@ -1,7 +0,0 @@
-def test_health(client):
-    res = client.get("/health")
-    assert res.status_code == 200
-
-def test_health_db(client):
-    res = client.get("/health/db")
-    assert res.status_code == 200
--- tests/unit/test_visit.py
+++
@@ -1,7 +0,0 @@
-from models import Visit
-
-def test_visit_created(app):
-    v = Visit.query.first()
-    assert v.patient_id > 0
-    assert v.doctor_id > 0
-    assert v.date is not None
