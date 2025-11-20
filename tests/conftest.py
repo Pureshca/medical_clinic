50e525cbaac6038687b01e91c7997ae1ad1a18ef
Pureshca <146196919+Pureshca@users.noreply.github.com>
2025-11-20 13:43:19 +0000

    fix_test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,28 +5,25 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 import pytest
-from app import app, db
+from app import app as flask_app, db as _db
 
 @pytest.fixture(scope="session")
 def test_app():
-    app.config["TESTING"] = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
-    app.config["WTF_CSRF_ENABLED"] = False
+    flask_app.config["TESTING"] = True
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"  # <-- in-memory
+    flask_app.config["WTF_CSRF_ENABLED"] = False
 
-    with app.app_context():
-        db.create_all()
-        yield app
-        db.session.remove()
-        db.drop_all()
+    with flask_app.app_context():
+        _db.create_all()
+        yield flask_app
+        _db.session.remove()
+        _db.drop_all()
 
 @pytest.fixture
 def db_session(test_app):
-    from app import db
-    yield db.session
-    db.session.rollback()
+    yield _db.session
+    _db.session.rollback()
 
-# <-- добавляем эту фикстуру
 @pytest.fixture
 def client(test_app):
     return test_app.test_client()
-
